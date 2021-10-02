373adeea955fcb19034b98b2cf950fe52a922445
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-10-02 10:40:39 +0300

    example 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 5;
-        switch (x % 2){
-            case 0:
-                x += 1;
-                break;
-            default:
-                x += 2;
-        }
+        int x = 1 + 1;
+        x = x + 1 + 2;
+        while(x % 2 != 0)
+            x += 1;
+        x += 1;
     }
 }
