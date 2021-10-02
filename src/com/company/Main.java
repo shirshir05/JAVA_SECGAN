310b3a0186b63adc5337e324c403da0755963c13
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-10-02 11:19:30 +0300

    if

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
         int x;
         x = 5;
-        while(x % 2 != 0)
-            x += 1;
-        x += 1;
+        if (x % 2 == 0)
+            x = x + 1;
+        else
+            x += 2;
     }
 }
