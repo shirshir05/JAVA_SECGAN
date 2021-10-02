eb51199fe898ac31258443cc27634205f3117501
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-10-02 10:32:46 +0300

    exmple 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int x;
-        x = 5;
-        if (x % 2 == 0)
-            x = x + 1;
-        else
-            x += 2;
+        int x = 5;
+        x += x % 2 == 0 ? 1 : 2;
     }
 }
