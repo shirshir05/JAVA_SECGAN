c84224463f4afda42cb3e4648baeb8eb6409a060
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-11-04 11:04:31 +0200

    if to while statements

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         int x = 5;
-        x = 5;
-        if (x % 2 == 0){
+        while(x % 2 != 0) {
             x += 1;
+            break;
         }
 
     }
