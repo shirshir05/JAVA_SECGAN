556fdfb8169ea4a9800fe4768ad0b8fb365af89e
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-11-04 16:25:58 +0200

    function

--- src/New.java
+++ src/New.java
@@ -1,8 +1,18 @@
 public class New {
 
     public static void main(String[] args) {
+        //ddd
+        //ddd
+        //ddd
+        //ddd
+        //ddd
         int i = 0;
         for(; i < 3; i++){
         }
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        int e = 0;
+
     }
 }
