9e89b8152d2144f582146efcfba2af24f2be28f9
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-11-04 11:05:58 +0200

    call function with two parameters

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        int x = 5;
-        while(x % 2 != 0) {
-            x += 1;
-            break;
-        }
+        m(1, 2);
 
     }
 }
