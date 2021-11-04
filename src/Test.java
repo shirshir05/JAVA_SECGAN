bd7917e410139c41806d755e7fb854e859a9a9f9
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-11-04 11:03:43 +0200

    if statements

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         int x = 5;
-        for(;x % 2 != 0;){
+        x = 5;
+        if (x % 2 == 0){
             x += 1;
-            break;
         }
 
     }
