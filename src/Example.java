1066198f47c272bde14c3b0e7108505e2da2777b
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-11-14 11:03:19 +0200

    base

--- src/Example.java
+++ src/Example.java
@@ -1,10 +1,6 @@
 public class Example {
 
     public static void main(String[] args) {
-        int x = 5;
-        for(;x % 2 != 0;){
-            x += 1;
-            break;
-        }
+
     }
 }
