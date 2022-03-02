c82f21e6e05ffe42514b92d3499dc7e51212ca86
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-02 13:04:37 +0200

    example a

--- src/Example.java
+++ src/Example.java
@@ -1,6 +1,11 @@
 public class Example {
 
     public static void main(String[] args) {
-
+        int x;
+        x = 5;
+        if (x%2 ==0)
+            x = x+ + 1;
+        else
+            x += 2;
     }
 }
