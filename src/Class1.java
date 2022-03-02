a456216d9f2b276006e34e6157371ec460275dec
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-02 13:10:00 +0200

    update class 1 and class 2

--- src/Class1.java
+++ src/Class1.java
@@ -1,10 +1,14 @@
 public class Class1 {
     public static void main(String[] args) {
         int x;
-        x = 5;
+        x = 4 + 1;
         if (x%2 ==0)
             x = x+ + 1;
         else
             x += 2;
+    }
+    public static Boolean test(int number) {
+        return number %2 ==0 ;
+
     }
 }
--- src/Class2.java
+++ src/Class2.java
@@ -2,7 +2,7 @@ public class Class2 {
     public static void main(String[] args) {
        int x = 1 + 1;
        x = x + 1 + 2;
-       while (x%2 !=0)
+       for (int i=0; x%2 !=0; )
            x +=1;
        x += 1;
     }
